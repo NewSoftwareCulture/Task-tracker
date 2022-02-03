de43fbd110a3845cd6f34dde361d5662e319d568
Vlados <vlados.pochta@gmail.com>
2022-02-04 00:56:22 +0400

    feat: add page wrapper

---
+++ client/src/app/components/common/PageWrapper/PageWrapper.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export function PageWrapper({ children }) {
+  return (
+    <div className="contaier">
+      <div className="row">
+        <div className="col-md-8 offset-md-2 bg-white">{children}</div>
+      </div>
+    </div>
+  );
+}
+
+PageWrapper.defaultProps = {
+  children: <div />,
+};
+
+PageWrapper.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]),
+};
+
+export default PageWrapper;
