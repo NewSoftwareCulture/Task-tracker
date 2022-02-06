51ccfdf74d2502265e637541934409394e69eff7
Vlados <vlados.pochta@gmail.com>
2022-02-07 02:34:15 +0400

    fix: 404 page

--- client/src/app/pages/notFoundPage.jsx
+++ client/src/app/pages/notFoundPage.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import { PageWrapper } from '../components/common/PageWrapper';
 
-export const notFoundPage = () => {
-  console.log('');
-  return (
-    <PageWrapper>
-      <h1>404</h1>
-      <h2>.</h2>
-    </PageWrapper>
-  );
-};
+export const notFoundPage = () => (
+  <PageWrapper>
+    <h1>404</h1>
+  </PageWrapper>
+);
 
 export default notFoundPage;
