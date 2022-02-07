15e523fc21d7c00a55697e6c675c884dca6098a1
Vlados <vlados.pochta@gmail.com>
2022-02-08 01:24:48 +0400

    fix: add FormWrapper sizes

--- client/src/app/components/common/FormWrapper/FormWrapper.jsx
+++ client/src/app/components/common/FormWrapper/FormWrapper.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export function FormWrapper({ children }) {
+export function FormWrapper({ size, children }) {
+  const sizes = {
+    sm: 'col-md-4 offset-md-4',
+    md: 'col-md-6 offset-md-3',
+    lg: 'col-md-8 offset-md-2',
+  };
+  const configuration = sizes[size] || sizes.md;
   return (
     <div className="container mt-5">
       <div className="row">
-        <div className="col-md-4 offset-md-4 shadow p-4 bg-white br-2 rounded">
+        <div className={`${configuration} shadow p-4 bg-white br-2 rounded`}>
           {children}
         </div>
       </div>
@@ -14,10 +20,12 @@ export function FormWrapper({ children }) {
 }
 
 FormWrapper.defaultProps = {
+  size: 'md',
   children: <div />,
 };
 
 FormWrapper.propTypes = {
+  size: PropTypes.string,
   children: PropTypes.oneOfType([
     PropTypes.arrayOf(PropTypes.node),
     PropTypes.node,
