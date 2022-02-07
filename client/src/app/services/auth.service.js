244fedbcd42bf72f3921a6e211776927951b2556
Vlados <vlados.pochta@gmail.com>
2022-02-08 01:28:27 +0400

    fix: auth service

--- client/src/app/services/auth.service.js
+++ client/src/app/services/auth.service.js
@@ -1,10 +1,26 @@
 import axios from 'axios';
+import get from 'lodash/get';
 import config from '../../.env.json';
+import { showAlert } from '../utils/showAlert';
 import { getRefreshToken } from './localStorage.service';
 
 const auth = axios.create({
   baseURL: `${config.apiEndpoint}auth/`,
 });
+
+auth.interceptors.response.use(
+  (res) => res,
+  (error) => {
+    const code = get(error, 'response.code');
+    const data = get(error, 'response.data');
+    const isExpectedErrors = code >= 400 && code < 500;
+
+    if (!isExpectedErrors) {
+      showAlert(data);
+    }
+    return Promise.reject(error);
+  },
+);
 
 export const authService = {
   signUp: async ({ email, password, name }) => {
